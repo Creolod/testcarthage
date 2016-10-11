e71fe285b3b96013bf3dca88788d421dd2b23da2
user <user@ws0580.local>
2016-10-11 18:30:45 +0600

    Initial Commit

---
+++ AMInfiniteScrolling/AMInfiniteScrolling.h
@@ -0,0 +1,19 @@
+//
+//  AMInfiniteScrolling.h
+//  AMInfiniteScrolling
+//
+//  Created by user on 11/10/16.
+//  Copyright © 2016 Academ Media. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AMInfiniteScrolling.
+FOUNDATION_EXPORT double AMInfiniteScrollingVersionNumber;
+
+//! Project version string for AMInfiniteScrolling.
+FOUNDATION_EXPORT const unsigned char AMInfiniteScrollingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMInfiniteScrolling/PublicHeader.h>
+
+
